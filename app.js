784690e1f441a71c0ded4db4e2c6edb5f05744a9
Shamiq Raza <155806245+ShamiqRaza32@users.noreply.github.com>
2024-02-23 22:02:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+let num1 = prompt ("Enter First No.");
+
+let num2 = prompt ("Enter second No.");
+
+//allowing string to be shified into
+num1 = Number(num1);
+num2 = Number(num2);
+
+//adition
+
+let sum = num1 + num2;
+alert("Addition: " + sum);
+console.log("Addition: " + sum)
+
+//substraction
+
+let minus = num1 - num2;
+alert("substraction: " + minus);
+console.log("substraction: " + minus);
+
+//multipilication
+
+let multiply = num1 * num2;
+alert("multipilication: " + multiply);
+console.log("multipilication: " + multiply);
+
+//Division
+
+let divide = num1 / num2;
+alert("Division: " + divide );
+console.log ("Division: " + divide);
